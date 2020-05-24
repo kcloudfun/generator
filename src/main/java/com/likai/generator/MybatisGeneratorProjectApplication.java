2b4093bc34b31ec2150cfdd9a847131ad2c56c68
likai <1414551714@qq.com>
2020-05-24 10:16:22 +0800

    初始版本。只支持mysql，数据库tingyint类型的转换比较不符合开发习惯

---
+++ src/main/java/com/likai/generator/MybatisGeneratorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.likai.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisGeneratorProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likai/generator/MybatisGeneratorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likai.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGeneratorProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
